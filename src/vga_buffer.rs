3bc6b70ee542aa1cd2664449c00431fc4730300f
programORdie <contact.programordie@gmail.com>
2025-06-28 22:50:54 +0200

    Test runner

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,4 @@
 use core::fmt;
-use core::fmt::Write;
 
 use lazy_static::lazy_static;
 use spin::Mutex;
@@ -144,4 +143,31 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn test_println_simple() {
+        println!("test_println_simple output");
+    }
+
+    #[test_case]
+    fn test_println_many() {
+        for _ in 0..100 {
+            println!("test_println_many output");
+        }
+    }
+
+    #[test_case]
+    fn test_println_output() {
+        let s = "Some test string that fits on a single line";
+        println!("{}", s);
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    }
 }
