d95391a30f87d55505e5c008f7cd58225615e64c
programORdie <contact.programordie@gmail.com>
2025-06-29 18:01:48 +0200

    Async

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,20 @@ use core::panic::PanicInfo;
 extern crate alloc;
 
 use bootloader::{BootInfo, entry_point};
-use goofy_os::{hlt_loop, memory::BootInfoFrameAllocator, println};
+use goofy_os::{
+    memory::BootInfoFrameAllocator,
+    println,
+    task::{Task, executor::Executor},
+};
 
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
 entry_point!(kernel_main);
 
@@ -30,36 +41,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
-
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
-
-    hlt_loop();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(goofy_os::task::keyboard::print_keypresses()));
+    executor.run();
 }
 
 #[cfg(not(test))]
